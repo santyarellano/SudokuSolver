0cef521a12269b2f2892d0effb0f5c5905f7b325
SSAM <ssam@MacBook-Pro-de-SSAM.local>
2020-09-12 22:37:52 -0500

    added animation to hover

--- funs.js
+++ funs.js
@@ -7,7 +7,7 @@ for (let row = 0; row < 9; row++) {
     let td = document.createElement("td");
     td.className = "sudoku_cell";
     td.id = "cell_" + row + "_" + col;
-    td.innerHTML = "0";
+    td.innerHTML = " ";
     tr.appendChild(td);
   }
   sudokuTable.appendChild(tr);
